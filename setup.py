95594fec2809ba0ef86d9318baa361401b605ae4
David Zhang <davzee@hotmail.com>
2015-03-15 22:21:42 -0400

    Add support for slave deletion, file browsing and ANSI colored status

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ except ImportError:
   from distutils.core import setup
 
 setup (name='easy-test',
-    version='1.6',
+    version='1.7',
     py_modules=['easy_test'],
     description='Easy test-running and editing of test input and solutions.',
     author='David Zhang',
     author_email='davzee@hotmail.com',
     url='http://github.com/davidozhang/easy-test',
-    install_requires=['pexpect==3.3'],
+    install_requires=['pexpect==3.3', 'termcolor==1.1.0'],
 )
